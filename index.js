14fe2aa6da47721e21a0b6661e26c332c98e4ecc
Anviti <39837967+anviti06@users.noreply.github.com>
2021-01-18 17:14:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ if (['production', 'ci'].includes(process.env.NODE_ENV)) {
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port`, PORT);
 });
